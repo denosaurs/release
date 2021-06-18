3348f5aeb1b7153f7ed35594bd8bb758cc446039
eliassjogreen <eliassjogreen1@gmail.com>
2021-06-18 21:10:06 +0200

    chore: bump deps

--- deps.ts
+++ deps.ts
@@ -1,15 +1,15 @@
-export * as log from "https://deno.land/x/branch@0.1.5/mod.ts";
-export { Spinner, wait } from "https://deno.land/x/wait@0.1.10/mod.ts";
+export * as log from "https://deno.land/x/branch@0.1.6/mod.ts";
+export { Spinner, wait } from "https://deno.land/x/wait@0.1.11/mod.ts";
 
 export type { Commit as CCCommit } from "https://deno.land/x/commit@0.1.5/mod.ts";
 export { parse as ccparse } from "https://deno.land/x/commit@0.1.5/mod.ts";
 
-export * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
+export * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
 export * as ini from "https://deno.land/x/ini@v2.1.0/mod.ts";
 
-export * as colors from "https://deno.land/std@0.88.0/fmt/colors.ts";
+export * as colors from "https://deno.land/std@0.99.0/fmt/colors.ts";
 
-export { readLines } from "https://deno.land/std@0.88.0/io/mod.ts";
-export { join } from "https://deno.land/std@0.88.0/path/mod.ts";
-export { ensureFile, exists } from "https://deno.land/std@0.88.0/fs/mod.ts";
-export { delay } from "https://deno.land/std@0.88.0/async/delay.ts";
+export { readLines } from "https://deno.land/std@0.99.0/io/mod.ts";
+export { join } from "https://deno.land/std@0.99.0/path/mod.ts";
+export { ensureFile, exists } from "https://deno.land/std@0.99.0/fs/mod.ts";
+export { delay } from "https://deno.land/std@0.99.0/async/delay.ts";
